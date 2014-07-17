e9da38fea04cf4094a317a814e0b0893380d2062
이정구 <hyde1004@gmail.com>
2014-07-18 08:39:37 +0900

    add method2

--- Lv1_Google/count8.py
+++ Lv1_Google/count8.py
@@ -7,9 +7,16 @@ def method1():
 			count += str(i).count("8")
 	return count
 
+def method2():
+	count = 0
+	for i in range(1, 10001):
+		count += str(i).count("8")
+	return count	
+
 class TestCount8(unittest.TestCase):
 	def test_methods(self):
 		self.assertEqual(method1(), 4000)
-
+		self.assertEqual(method2(), 4000)
+		
 if __name__ == "__main__":
 	unittest.main()
